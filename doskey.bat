bf1c4ea322669c87593502b4473acc0a6cb9d8af
Jeff Powell <cpu3140@gmail.com>
2017-12-01 11:31:19 -0700

    Added some new aliases and organization

--- doskey.bat
+++ doskey.bat
@@ -4,14 +4,26 @@ REM 2 navigate to the HKEY_CURRENT_USER\SOFTWARE\Microsoft\Command Processor key
 REM 3 add a new string value called "AutoRun" and set the value to "U:\helpful code\doskey.bat"
 
 SET MYUSER="C:\dev\user\jeffpowell\"
+SET LOGFORMAT="%%h < %%p: %%s (%%an)"
 
+REM Navigation
 doskey cdam=cd C:\dev\projects\asset-manager-tools\$*
 doskey cdamw=cd C:\dev\projects\asset-manager-tools\asset-manager-web\$*
-doskey cdpuppet=cd C:\puppet\puppet ^& git pull
-doskey restartnginx=call "U:\helpful code\nginx.bat"
+doskey cdpuppet=cd C:\puppet\puppet $T git pull
 doskey myuser=cd %MYUSER%$* $T dir /B
-doskey build=npm install ^& npm prune ^& npm run dev-build
 
+REM Starting/Stopping applications
+doskey restartnginx=call "U:\helpful code\nginx.bat"
+doskey stoptomcat=call "U:\helpful code\skinthecat.bat"
+doskey memcached=call "U:\helpful code\memcached.bat"
+doskey startmongo=start mongod --auth --dbpath /data/db
+
+REM File Management
+doskey build=npm install $T npm prune $T npm run dev-build
+doskey puppetb=call "U:\helpful code\puppetBranch.bat" $1
+doskey gitgraph=git log --graph --pretty=format:%LOGFORMAT%
+
+REM Table of Contents
 doskey aliases=doskey /macros
 
 REM echo. & echo Doskey macros applied; type 'aliases' for a list of options
